dd185d9fb20b0732b1bd1799084145014f5e2c58
Angkasawari Sakti <101614752+angkswrsktii@users.noreply.github.com>
2025-10-23 19:27:15 +0700

    refactor: [Sakti] Refactor all endpoint

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,31 @@ use App\Http\Controllers\ProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('users', UserController::class);
-Route::apiResource('suppliers', SupplierController::class);
-Route::apiResource('sales', SalesController::class);
-Route::apiResource('products', ProductController::class);
+
+// === ROUTES UNTUK USER ===
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// === ROUTES UNTUK PRODUCTS ===
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+// === ROUTES UNTUK SUPPLIERS ===
+Route::get('/suppliers', [SupplierController::class, 'index']);
+Route::get('/suppliers/{id}', [SupplierController::class, 'show']);
+Route::post('/suppliers', [SupplierController::class, 'store']);
+Route::put('/suppliers/{id}', [SupplierController::class, 'update']);
+Route::delete('/suppliers/{id}', [SupplierController::class, 'destroy']);
+
+// === ROUTES UNTUK SALES ===
+Route::get('/sales', [SalesController::class, 'index']);
+Route::get('/sales/{id}', [SalesController::class, 'show']);
+Route::post('/sales', [SalesController::class, 'store']);
+Route::put('/sales/{id}', [SalesController::class, 'update']);
+Route::delete('/sales/{id}', [SalesController::class, 'destroy']);
